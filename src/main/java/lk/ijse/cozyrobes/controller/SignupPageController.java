f25b7a4deef5f92c6148839b3ccfe01314705935
Sarasi <sarasividumini13@gmail.com>
2025-06-02 08:01:53 +0530

    refactor: modified Sign up view

--- src/main/java/lk/ijse/cozyrobes/controller/SignupPageController.java
+++ src/main/java/lk/ijse/cozyrobes/controller/SignupPageController.java
@@ -3,6 +3,7 @@ package lk.ijse.cozyrobes.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -23,6 +24,15 @@ public class SignupPageController {
     }
 
     public void btnGoDashBoardPageOnAction(ActionEvent actionEvent) throws IOException {
+        String userName = txtUserName.getText();
+        String contact = txtContact.getText();
+        String password = txtPassword.getText();
+
+        if (userName.isEmpty() || contact.isEmpty() || password.isEmpty()) {
+            new Alert(Alert.AlertType.ERROR, "Empty Fields").show();
+            return;
+        }
+
         ancSignupPage.getChildren().clear();
         Parent load = FXMLLoader.load(getClass().getResource("/view/DashBoardPage.fxml"));
         ancSignupPage.getChildren().add(load);
